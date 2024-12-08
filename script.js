f4990ad225f429e9d3ab023bd080cfe677e71573
navyaanzaheen <navyaanzaheen@gmail.com>
2024-12-08 21:38:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function animal1(){
+    alert("This is Dog Image");
+}
+
+
+function animal2(){
+    alert("This is Horse Image");
+}
+
+function animal3(){
+    alert("This is Buffalo Image");
+}
+
+function animal4(){
+    alert("This is Zebra Image");
+}
+
+
+function animal5(){
+    alert("This is Lion Image");
+}
+
+
+function animal6(){
+    alert("This is Girafffe Image");
+}
+
+function animal7(){
+    alert("This is Tiger Image");
+}
+
+function animal8(){
+    alert("This is Camel Image");
+}
+
+
